fc8999c16df0026cb6c31602a1bbf27a10a278dc
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-09 17:23:58 -0400

    Added Specialties to Vets on Startup. Closes #34

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Component;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.model.Specialty;
 import guru.springframework.sfgpetclinic.model.Vet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import guru.springframework.sfgpetclinic.services.SpecialtyService;
 import guru.springframework.sfgpetclinic.services.VetService;
 
 @Component
@@ -19,17 +21,30 @@ public class DataLoader implements CommandLineRunner {
 	private final OwnerService ownerService;
 	private final VetService vetService;
 	private final PetTypeService petTypeService;
+	private final SpecialtyService specialtyService;
 	
-	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
+			SpecialtyService specialtyService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
 		this.petTypeService = petTypeService;
+		this.specialtyService = specialtyService;
 	}
 
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		
+		int count = petTypeService.findAll().size();
+		
+		if(count == 0) {
+			loadData();
+		}		
+	}
+
+
+	private void loadData() {
 		PetType dog = new PetType();
 		dog.setName("Dog");
 		PetType savedDogPetType = petTypeService.save(dog);
@@ -68,26 +83,47 @@ public class DataLoader implements CommandLineRunner {
 		fiona.getPets().add(fionasCat);
 		ownerService.save(fiona);
 		
-		System.out.println("Loaded Owners....");
-		
-		Vet vet1 = new Vet();
-		vet1.setFirstName("Sam");
-		vet1.setLastName("Axe");
-		
-		vetService.save(vet1);
-		
-		Vet vet2 = new Vet();
-		vet2.setFirstName("Jessie");
-		vet2.setLastName("Porter");
-		
-		vetService.save(vet2);
+		System.out.println("--------Loaded Owners---------");
 		
 		
-		System.out.println("Loaded Vets...");
+		/****** Specialty Objects *******/
+		Specialty radiology = new Specialty();
+		radiology.setDescription("Radiology");
+		Specialty savedRadiology = specialtyService.save(radiology);
 		
+		Specialty surgery = new Specialty();
+		surgery.setDescription("Suregery");
+		Specialty savedSurgery = specialtyService.save(surgery);
+		
+		Specialty dentistry = new Specialty();
+		dentistry.setDescription("Dentistry");
+		Specialty savedDentistry = specialtyService.save(dentistry);
 		
 		
 		
+		/****** Vet Objects *******/
+		Vet sam = new Vet();
+		sam.setFirstName("Sam");
+		sam.setLastName("Axe");
+		sam.getSpecialties().add(savedRadiology);
+		
+		vetService.save(sam);
+		
+		Vet jessie = new Vet();
+		jessie.setFirstName("Jessie");
+		jessie.setLastName("Porter");
+		jessie.getSpecialties().add(savedSurgery);
+		
+		vetService.save(jessie);
+		
+		Vet harry = new Vet();
+		harry.setFirstName("Harry");
+		harry.setLastName("Trump");
+		harry.getSpecialties().add(savedDentistry);
+		
+		vetService.save(harry);
+		
+		System.out.println("--------Loaded Vets---------");
 	}
 
 	
