826db303a8ef3326d3cd9ab5f4d70870b858b3df
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-14 03:04:48 -0400

    Added Specialty Spring Data Jpa Services, closes #44

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/mapImpl/PerMapService.java
@@ -0,0 +1,35 @@
+package guru.springframework.sfgpetclinic.services.mapImpl;
+
+import java.util.Set;
+import org.springframework.stereotype.Service;
+import guru.springframework.sfgpetclinic.model.Pet;
+import guru.springframework.sfgpetclinic.services.PetService;
+@Service
+public class PerMapService extends AbstractMapService<Pet, Long> implements PetService{
+
+	@Override
+	public Set<Pet> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Pet findById(Long id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Pet save(Pet object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void delete(Pet object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);		
+	}
+
+}
