c9b28045cff4a285dc5d9cec08a1f3c285e4c4b6
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-14 03:51:05 -0400

    Added Visit Map Service, closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * Created by tairovich_jr on Sep 14, 2020
+ */
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
