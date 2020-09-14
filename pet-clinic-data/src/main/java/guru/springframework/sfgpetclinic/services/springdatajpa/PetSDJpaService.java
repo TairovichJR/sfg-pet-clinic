20b59515584206345d28afee28cf876940bbd4b2
Omonjon Yokubov <yokubov.omonjon@gmail.com>
2020-09-14 02:52:04 -0400

    Added Spring data jpa service for pet type service, closes #42

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/PetSDJpaService.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by tairovich_jr on Sep 14, 2020
+ */
+@Service
+@Profile("springdatajpa")
+public class PetSDJpaService {
+
+}
